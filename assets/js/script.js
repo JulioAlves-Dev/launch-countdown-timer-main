3ac36c6a463c87dad304141ccc9b6df049dad5da
Julio Alves <73007273+JulioAlves-Dev@users.noreply.github.com>
2023-12-17 01:46:39 -0300

    Atualizando data de lançamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const QS = (e) => document.querySelector(e),
   dataH = QS("main .times span[data-horas]"),
   dataM = QS("main .times span[data-minutos]"),
   dataS = QS("main .times span[data-segundos]"),
-  lanca = new Date("December 29 2021"),
+  lancamento = new Date("December 29 2024"),
   d = (time) => time / (24 * 60 * 60 * 1000),
   h = (time) => time / (60 * 60 * 1000),
   m = (time) => time / (60 * 1000),
@@ -13,10 +13,10 @@ let atual = new Date();
 
 function atualizarData() {
   atual = new Date();
-  dataD.innerText = Math.floor(d(lanca) - d(atual));
-  dataH.innerText = Math.floor((h(lanca) - h(atual)) % 24);
-  dataM.innerText = Math.floor((m(lanca) - m(atual)) % 60);
-  dataS.innerText = Math.floor((s(lanca) - s(atual)) % 60);
+  dataD.innerText = Math.floor(d(lancamento) - d(atual));
+  dataH.innerText = Math.floor((h(lancamento) - h(atual)) % 24);
+  dataM.innerText = Math.floor((m(lancamento) - m(atual)) % 60);
+  dataS.innerText = Math.floor((s(lancamento) - s(atual)) % 60);
 }
 atualizarData();
 
